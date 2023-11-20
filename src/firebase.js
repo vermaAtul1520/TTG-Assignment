d4af49ba4943f0d014eeabeeb08db7104a375d67
brimoAtul1520 <atul.v@brimo.in>
2023-11-20 19:38:50 +0530

    key added to env

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import {
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    // authDomain: "firstone-5ac2a.firebaseapp.com",
-    // projectId: "firstone-5ac2a",
-    // storageBucket: "firstone-5ac2a.appspot.com",
-    // messagingSenderId: "10842926773",
-    // appId: "1:10842926773:web:31ef97c5365f62373398fc",
-    // measurementId: "G-T0DX9P5KGC",
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     apiKey: "AIzaSyB784cU5hAAmDBZrWwYD6CjNgsUGAVzNNY",
     authDomain: "shop-list-288e5.firebaseapp.com",
     projectId: "shop-list-288e5",
@@ -22,6 +16,8 @@ const firebaseConfig = {
     messagingSenderId: "80414812095",
     appId: "1:80414812095:web:42c7bbf59f87c2df4ea30a"
 };
+
+console.log("wer",process.env)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
